6e742d3c9a747372b065635a8a541f4a30159f8a
fwehn <finn.wehn@web.de>
2023-05-25 20:38:35 +0200

    added copyright notice to all files;

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+/*
+This file is part of G.E.C.K.O.
+Copyright (C) 2023  Finn Wehn
+
+G.E.C.K.O. is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 let configObject = {
     sayFunction: () => {},
-};
-
-let sessionObject = {
-    siteId: "",
-    sessionId: "",
-    answers: [],
+    answerFunction: () => {},
 };
 
 function setConfigData(data) {
@@ -16,20 +28,16 @@ function setConfigData(data) {
     }
 }
 
-function setSessionData(data) {
-    for (let i in sessionObject) {
-        if (Object.prototype.hasOwnProperty.call(data, i) && typeof data[i] === typeof sessionObject[i]) {
-            sessionObject[i] = data[i];
-        }
-    }
-}
-
 function say(message = "") {
     configObject["sayFunction"](message);
 }
 
+function generateAnswer(answerIndex, ...args) {
+    return configObject["answerFunction"](answerIndex, ...args);
+}
+
 module.exports = {
     setConfigData,
-    setSessionData,
     say,
+    generateAnswer,
 };
